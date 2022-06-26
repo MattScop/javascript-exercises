63f9c25b6960bb7dd9922a9933fc9568def59410
MattScop <matteo.scopece@outlook.com>
2022-06-26 10:29:22 +0200

    Add solution exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...otherNum) {
+    nextNum: for (let i = 0; i < otherNum.length; i++) {
+        if (arr.includes(otherNum[i])) {
+            let index = arr.indexOf(otherNum[i]);
+            arr.splice(index, 1);
+        } else {
+            continue nextNum;
+        }
+    }      
+    return arr
 };
 
 // Do not edit below this line
