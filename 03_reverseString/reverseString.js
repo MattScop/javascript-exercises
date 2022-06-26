15960e265bd6c86e8161233b5ddb163690af1fb0
MattScop <matteo.scopece@outlook.com>
2022-06-26 09:49:07 +0200

    Add solution exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitStr = string.split("");
+    let reverseStr = splitStr.reverse();
+    let strJoin = reverseStr.join("");
 
+    return strJoin;
 };
 
 // Do not edit below this line
