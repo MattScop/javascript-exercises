eaaa8aeb1efc6e3219c7fb98770040b98cbb5ab4
MattScop <matteo.scopece@outlook.com>
2022-06-28 09:42:01 +0200

    Add solution exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
+/* ftoc(number) => (number -32) * .5556 
+   ctof(number) => (number * 1.8) + 32
+*/
 
+const ftoc = function (number) {
+  let convertedTemp = (number - 32) * 0.5556;
+  if (convertedTemp === 0) {
+    return convertedTemp;
+  } else {
+    return parseFloat(convertedTemp.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (number) {
+  let convertedTemp = (number * 1.8) + 32;
+  if (Number.isInteger(convertedTemp)) {
+    return convertedTemp
+  } else {
+    return parseFloat(convertedTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
