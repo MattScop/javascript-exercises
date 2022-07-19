5e734447dda059f7fe3868632b64eb385cd2589b
MattScop <matteo.scopece@outlook.com>
2022-07-19 09:17:22 +0200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((totalValue, currentValue) => {
+      return totalValue + currentValue
+    }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((totalMultiply, currentMultiply) => {
+    return totalMultiply * currentMultiply
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  let total = 1;
+  for (let b = a; b > 0; b--) {
+    total *= b
+  }
+  return total
 };
 
 // Do not edit below this line
