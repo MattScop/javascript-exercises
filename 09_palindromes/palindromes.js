bef38bdf0ad463d15834c70b24530d573ff2ce7a
MattScop <matteo.scopece@outlook.com>
2022-07-19 11:44:01 +0200

    Add solution exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {    
+    const joined = (Array.from(string).filter(singleLetter => {
+        if (singleLetter.match(/[a-zA-Z]/)) return true
+    }));
 
+    const newArr = [];
+    
+    for (let i = 0, z = joined.length - 1; i < joined.length; i++, z--) {
+        if (joined[i].toLowerCase().match(joined[z].toLowerCase()))  {
+            newArr.push(joined[i])
+        } 
+    }
+
+    return (newArr.join('') === joined.join('')) ? true : false
 };
 
 // Do not edit below this line
