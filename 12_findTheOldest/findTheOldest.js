d2bc637ff6e63c23aec454de915addf1d2d25050
MattScop <matteo.scopece@outlook.com>
2022-07-20 10:52:31 +0200

    Add solution exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const newObj = array.map(person => {
+        const obj = {};
+        obj.name = person.name;
+        if (person.yearOfDeath) {
+            obj.lifespan = person.yearOfDeath - person.yearOfBirth;
+            return obj
+        } else {
+            currentYear = new Date().getFullYear();
+            obj.lifespan = currentYear - person.yearOfBirth;
+            return obj
+        }
+    }).sort((a,b) => {
+        return b.lifespan - a.lifespan
+    });
+    return newObj[0]
 };
 
 // Do not edit below this line
