1c8a060119298a3ab9942dc2e6e5b2ade9f27a35
MattScop <matteo.scopece@outlook.com>
2022-07-20 09:31:54 +0200

    Add solution exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    // const bookArr = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //      bookArr.push(arr[i].title)
+    // }
+    // return bookArr
 
+    // METHOD 2
+    return arr.map(book => book.title)
 };
 
 // Do not edit below this line
