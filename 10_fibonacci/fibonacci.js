3576abf98c45b769cabd41582c1d84bc8b355fe0
MattScop <matteo.scopece@outlook.com>
2022-07-20 08:43:56 +0200

    Add exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // fibo n = (n-1) + (n-2)
+    if (n < 0) return "OOPS"
+    const fiboArray = [];
+    
+    for (let i = 0; i < n; i++) {
+        const fiboNumber = () => {
+            switch (true) {
+                case i-1 <= 0:
+                    return 1
+            
+                case i-2 <= 0:
+                    return (i-1) + 1
 
+                default:
+                    return fiboArray[i-1] + fiboArray[i-2]
+            }
+        }
+        fiboArray.push(fiboNumber());
+    }
+    return fiboArray[n-1]
 };
 
 // Do not edit below this line
